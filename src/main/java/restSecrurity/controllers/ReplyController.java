ccaac99d7acb1e72b4d1ece683e6f6f2d616d9ef
Odiegenx <124783324+Odiegenx@users.noreply.github.com>
2024-05-29 15:26:29 +0200

    edit reply endpoint added

--- src/main/java/restSecrurity/controllers/ReplyController.java
+++ src/main/java/restSecrurity/controllers/ReplyController.java
@@ -4,8 +4,13 @@ import io.javalin.http.Handler;
 import restSecrurity.DOA.databaseDAO.ReplyDAO;
 import restSecrurity.DOA.databaseDAO.ThreadDAO;
 import restSecrurity.DOA.iDAO;
+import restSecrurity.DTO.PostDTO;
+import restSecrurity.DTO.ReplyDTO;
+import restSecrurity.exceptions.ApiException;
+import restSecrurity.persistance.Post;
 import restSecrurity.persistance.Reply;
 import restSecrurity.persistance.Thread;
+import utills.Updater;
 
 public class ReplyController {
     private static iDAO<Reply, Integer> replyDAO;
@@ -25,7 +30,18 @@ public class ReplyController {
 
     public Handler editReplyById() {
         return ctx -> {
-            
+            try {
+                int id = Integer.parseInt(ctx.pathParam("id"));
+                ReplyDTO toUpdateWithDTO = ctx.bodyAsClass(ReplyDTO.class);
+                Reply toUpdateFrom = replyDAO.getById(id);
+                Reply toUpdate = Updater.updateFromDTO(toUpdateFrom, toUpdateWithDTO);
+                Reply updated = replyDAO.update(toUpdate, id);
+                ReplyDTO updatedThreadDTO = new ReplyDTO(updated);
+                ctx.json(updatedThreadDTO);
+            } catch (Exception e) {
+                ctx.status(500);
+                ctx.json(new ApiException(500, "Error while editing reply: " + e.getMessage()));
+            }
         };
     }
 }
