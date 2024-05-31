2331e6fb7beeb296f98aa2098882194fd2e752ab
Odiegenx <124783324+Odiegenx@users.noreply.github.com>
2024-05-31 22:46:00 +0200

    fixed sort bug

--- src/main/java/restSecrurity/DOA/databaseDAO/DAO.java
+++ src/main/java/restSecrurity/DOA/databaseDAO/DAO.java
@@ -55,7 +55,7 @@ public abstract class DAO<T, D> implements iDAO<T,D> {
     }
     public List<T> getAll() {
         try(EntityManager em = emf.createEntityManager()) {
-            TypedQuery<T> query = em.createQuery("SELECT h from "+objectClass.getSimpleName()+" h ORDER BY h.createdDate DESC",objectClass);
+            TypedQuery<T> query = em.createQuery("SELECT h from "+objectClass.getSimpleName()+" h",objectClass);
             List<T> queryList = query.getResultList();
             return queryList;
         }
